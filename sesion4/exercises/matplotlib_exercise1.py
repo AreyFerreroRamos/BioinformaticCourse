334929fd0793adf78ab7229c0714d147234f9257
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2024-07-26 18:31:22 +0200

    Arey: Ending exercise pandas and matplotlib. Add files sesion 5.

--- sesion4/exercises/matplotlib_exercise1.py
+++ sesion4/exercises/matplotlib_exercise1.py
@@ -1,19 +1,26 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
+import sys
 
 def calculate_imc(df):
-    df["imc"] = df["Weight"] / (df["Height"] * df["Height"])
+    df["Imc"] = df["Weight"] / (df["Height"] * df["Height"])
     return df
 
 if __name__ == "__main__":
-    
-    df_biostats = pd.read_csv("biostats.csv")
-
+    df_biostats = pd.read_csv(sys.argv[1])
     df_biostats = df_biostats.dropna()
+    print(df_biostats)
 
     df_imc = calculate_imc(df_biostats)
     print(df_imc)
 
     grouped_df_imc = df_imc.groupby("Sex")
-    df_mean_imc = grouped_df_imc.agg(Mean_imc=("imc", "mean"))
+    df_mean_imc = grouped_df_imc.agg(Mean_imc=("Imc", "mean"))
     print(df_mean_imc)
+
+    df_mean_imc = df_mean_imc.reset_index()
+    print(df_mean_imc)
+
+    plt.bar(x=df_mean_imc["Sex"], height=df_mean_imc["Mean_imc"])
+    plt.show()
+    
