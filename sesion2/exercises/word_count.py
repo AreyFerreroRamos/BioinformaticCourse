96e83be6e69085a3e6cccdfc2acd07166c37ecf1
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2024-07-30 18:28:41 +0200

    Arey: Sesion 2: Ending of the exercise related to the implementation of a function for counting words and showed it ordered.

--- sesion2/exercises/word_count.py
+++ sesion2/exercises/word_count.py
@@ -1,3 +1,6 @@
+def get_freq(lst):
+    return lst[1]
+
 def word_count(lst):
     count_results = {}
     ordered_words = []
@@ -8,15 +11,15 @@ def word_count(lst):
         else:
             count_results[lst[i]] = 1
 
-    def get_freq(lst):
-        return lst[1]
-
     sorted_vals = sorted(count_results.items(), key=get_freq, reverse=True)
-    sorted_key = [ v[0] for v in sorted_vals ]    
+    ordered_words = [val[0] for val in sorted_vals]
 
     return count_results, ordered_words
 
 if __name__ == "__main__":
-    lst = [ "poto", "platanero", "aloe", "aloe", "aloe", "cactus", "platanero", "poto", "suculenta", "platanero", "aloe", "aloe", "poto","ficus" , "suculenta" ]
+    lst = ["poto", "platanero", "aloe", "aloe", "aloe", "cactus", "platanero", "poto", "suculenta", "platanero", "aloe", "aloe", "poto", "ficus", "suculenta"]
     
-    print(word_count(lst))
+    count_results, ordered_words = word_count(lst)
+    print(count_results)
+    print(ordered_words)
+
