3d459a762c726eeafc0172c5ec24fb8d3cd08217
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2024-08-02 19:12:11 +0200

    Arey: Sesion 4: Revision of the matplotlib exercise.

--- sesion4/exercises/matplotlib_exercises1.py
+++ sesion4/exercises/matplotlib_exercises1.py
@@ -2,15 +2,19 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import sys
 
+
 def calculate_imc(df):
     df["Imc"] = df["Weight"] / (df["Height"] * df["Height"])
     return df
 
+
 if __name__ == "__main__":
+    # Load data from 'biostats.csv' file and delete rows with any inexistant entry (NaN).
     df_biostats = pd.read_csv(sys.argv[1])
     df_biostats = df_biostats.dropna()
     print(df_biostats)
 
+    # Calculate the IMC mean in men and women (separately).
     df_imc = calculate_imc(df_biostats)
     print(df_imc)
 
@@ -21,6 +25,7 @@ if __name__ == "__main__":
     df_mean_imc = df_mean_imc.reset_index()
     print(df_mean_imc)
 
+    # Represent the IMC means in a bar plot.
     plt.bar(x=df_mean_imc["Sex"], height=df_mean_imc["Mean_imc"])
     plt.show()
     
