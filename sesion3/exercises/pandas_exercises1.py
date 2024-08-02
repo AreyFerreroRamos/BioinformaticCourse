0a2cbc7f3e297fa7683450361e03a211117474d2
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2024-08-02 18:24:22 +0200

    Arey: Sesion 3: Ending the pandas exercise 1.

--- sesion3/exercises/pandas_exercises1.py
+++ sesion3/exercises/pandas_exercises1.py
@@ -1,12 +1,29 @@
 import pandas as pd
-
-# Load data from biostats.csv file.
-df = pd.read_csv("biostats.csv")
-
-# Delete rows with any inexistant entry (NaN).
-df = df.dropna()
-print(df)
+import sys
 
 
-mean_weight = df[df["Sex"] == "M"]["Weight"].mean()
+def calculate_imc(df):
+    df["IMC"] = df["Weight"] / (df["Height"] * df["Height"])
+    return df
 
+
+if __name__ == "__main__":
+    # Load data from a file.
+    df = pd.read_csv(sys.argv[1])
+    print(df)
+
+    # Delete rows with any inexistant entry (NaN).
+    df = df.dropna()
+    print(df)
+
+    # Calculate the IMC of each individual.
+    print(calculate_imc(df))
+
+    # Calculate the mean weight in men.
+    mean_weight_men = df[df["Sex"] == "M"]["Weight"].mean()
+    print(mean_weight_men)
+
+    # Calculate standard desviation in height in women greater than 35 years. 
+    # The standard desviation is NaN beacuse there is only one element.
+    std_desv_height = df[(df["Sex"] == "F") & (df["Age"] > 35)]["Height"].std()
+    print(std_desv_height)
